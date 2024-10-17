68088199ae670b401ded55faddf67ac7ce97cab4
electricprogramming <yeungj316@gmail.com>
2024-10-17 15:42:21 -0400

    Update JavaScript.js

--- nativeForScratch/JavaScript.js
+++ nativeForScratch/JavaScript.js
@@ -1,319 +1,326 @@
-const from_s = s => ("" + s).split(" ").map(s => s.split(",").map(v => (isNaN(+v) ? 0 : +v)));
-const to_s = m => m.map(v => v.join(",")).join(" ");
-const safe_index = (m, i) => m[m.length == 1 ? 0 : i];
-const component_wise = f => (a, b) => {
-  let [m, n] = a.length > b.length ? [a, b] : [b, a];
-  return m.map((v, i) => safe_index(n, i) == undefined ? v : f(safe_index(a, i), safe_index(b, i)));
-};
-var cursor_x = -1;
-var cursor_y = -1;
-document.onmousemove = function(event) {
-  cursor_x = event.pageX;
-  cursor_y = event.pageY;
-};
-const component_wise2D = f => (a, b) => component_wise(component_wise(f))(a, b);
-const add = component_wise((a, b) => a + b);
-const mul = component_wise((a, b) => a * b);
-const add2D = component_wise2D((a, b) => a + b);
-const sub2D = component_wise2D((a, b) => a - b);
-const mul2D = component_wise2D((a, b) => a * b);
-const div2D = component_wise2D((a, b) => a / b);
-const set = (i, m, v) => {
-  if (m.length == 1) {
-    if (m[0][i - 1] != undefined) m[0][i - 1] = v[0][0];
-  } else {
-    m[i - 1] = v[0];
+(function(){
+  let lastOpenedFileData = {name: '', extension: '', size: 0, contentAsText: '', contentAsDataURL: ''};
+  function promptForFile(acceptedExtensions = []) {
+    return new Promise((resolve, reject) => {
+      const input = document.createElement('input');
+      input.type = 'file';
+      if (acceptedExtensions.length > 0) {
+        input.accept = acceptedExtensions.map(ext => ext).join(',');
+      }
+      input.onchange = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+          readFileData(file).then(resolve).catch(reject);
+        } else {
+          reject(new Error('No file selected.'));
+        }
+      };
+      input.click();
+    });
   }
-  return m;
-};
-const get = (i, m) => [m.length == 1 ? [m[0][i - 1]] ?? [] : m[i - 1] ?? []];
-const dot = (a, b) => {
-  if (a.length == 1 && a[0].length == 3) a = a[0].map(v => [v]);
-  return b.map(bv => a.reduce((acc, av, i) => add(acc, mul(av, [safe_index(bv, i)])), 0));
-};
-const det = (m, i1, i2) => m[0][i1 % 3] * m[1][i2 % 3] - m[0][i2 % 3] * m[1][i1 % 3];
-const cross = component_wise((a, b) => a.map((_, i) => det([a, b], i + 1, i + 2)));
-const length = m => m.map(v => dot([v], [v]).map(v => Math.sqrt(v)));
-const normalize = m => div2D(m, length(m));
-const rotate = (a, v) => {
-  a = (a[0][0] * Math.PI) / 180;
-  v = normalize(v)[0];
-  const s = Math.sin(a);
-  const c = Math.cos(a);
-  const f1 = i => c + v[i] * v[i] * (1 - c);
-  const f2 = (i, n) => v[(i + 1) % 3] * v[(i + 2) % 3] * (1 - c) + n * v[i] * s;
-  return [
-    [f1(0), f2(2, 1), f2(1, -1)],
-    [f2(2, -1), f1(1), f2(0, 1)],
-    [f2(1, 1), f2(0, -1), f1(2)],
-  ];
-};
-const letter = i => String.fromCharCode(97 + i);
-const auto_block = (blockType, opcode, text, args) => ({
-  blockType,
-  opcode,
-  text,
-  arguments: Object.fromEntries(new Array(text.split("[").length - 1).fill().map((_, i) => [letter(i), {
-    type: (args && args[i]) || "string",
-    defaultValue: " "
-  }, ])),
-  hideFromPalette: !0,
-});
-const mat_reporter_f = f => o => to_s(f(...new Array(Object.entries(o).length).fill().map((_, i) => from_s(o[letter(i)]))));
-class JavaScriptExt {
-  constructor(runtime) {
-    this.runtime = runtime;
+  function readFileData(file) {
+    return new Promise((resolve, reject) => {
+      const readerText = new FileReader();
+      const readerDataURL = new FileReader();
+      readerText.onload = (event) => {
+        const contentAsText = event.target.result;
+        readerDataURL.onload = (event) => {
+          const contentAsDataURL = event.target.result;
+          resolve({
+            name: file.name,
+            extension: `.${file.name.split('.').pop()}`,
+            size: file.size,
+            contentAsText: contentAsText,
+            contentAsDataURL: contentAsDataURL,
+          });
+        };
+        readerDataURL.onerror = (error) => {
+          reject(new Error('Error reading file as Data URL: ' + error));
+        };
+        readerDataURL.readAsDataURL(file);
+      };
+      readerText.onerror = (error) => {
+        reject(new Error('Error reading file as text: ' + error));
+      };
+      readerText.readAsText(file);
+    });
   }
-  RunJS({
-    code
-  }) {
-    eval(code);
-  }
-  OpenSite({
-    url
-  }) {
-    window.open(url);
-  }
-  SaveFile({
-    name,
-    contents
-  }) {
-    const a = document.createElement("a");
-    a.download = name;
-    a.href = `data:text/plain;charset=utf-8,${encodeURIComponent(contents)}`;
+  function _downloadFile(fileName, content) {
+    const blob = new Blob([content], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    document.body.appendChild(a);
     a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
   }
-  setVar({
-    name,
-    val
-  }) {
-    eval(`${name}="${val}";`);
-  }
-  getReturnValOfJS({
-    code
-  }) {
-    return eval(code);
-  }
-  stringReport({
-    arg1
-  }) {
-    return arg1;
-  }
-  whenCondition({
-    condit
-  }) {
-    return Boolean(condit);
-  }
-  ifBoolStringElseString({
-    arg1,
-    arg2,
-    arg3
-  }) {
-    return arg1 ? arg2 : arg3;
-  }
-  outOfBoundsMouseX() {
-    return cursor_x;
-  }
-  outOfBoundsMouseY() {
-    return cursor_y;
-  }
-  getInfo() {
-    return {
-      id: "epJsForScratch",
-      name: "JavaScript",
-      blocks: [auto_block("reporter", "Vec", "vector [a] [b] [c]"), auto_block("reporter", "Arr", "list [a] [b]"), auto_block("reporter", "Get", "item [a] of [b]"), auto_block("reporter", "Set", "with item [a] of [b] = [c]"), auto_block("reporter", "Rot", "rotate [a] around [b]", ["angle"]), {
-        blockType: "command",
-        opcode: "Out",
-        text: "output [a] to [b]",
-        arguments: {
-          a: {
-            type: "number",
-            defaultValue: " "
+  class JSforScratch {
+    getInfo() {
+      return {
+        id: "jsForScratch",
+        name: "JavaScript",
+        blocks: [
+          {
+            blockType: "command",
+            opcode: "commandJS",
+            text: "js [code]",
+            arguments: {
+              code: {
+                type: "string",
+                defaultValue: "alert('Hello World!')"
+              },
+            },
           },
-          b: {
-            type: "string",
-            defaultValue: " ",
-            menu: "varMenu"
+          {
+            blockType: "reporter",
+            opcode: "reporterJS",
+            text: "js [code]",
+            arguments: {
+              code: {
+                type: "string",
+                defaultValue: "prompt('What is your name?','Bob Smith')"
+              },
+            },
           },
-        },
-        hideFromPalette: !0,
-      }, auto_block("reporter", "Add", "[a] + [b]"), auto_block("reporter", "Sub", "[a] - [b]"), auto_block("reporter", "Mul", "[a] * [b]"), auto_block("reporter", "Div", "[a] / [b]"), auto_block("reporter", "Dot", "[a] dot [b]"), auto_block("reporter", "Cross", "[a] cross [b]"), auto_block("reporter", "Len", "length of [a]"), auto_block("reporter", "Norm", "normalize [a]"), auto_block("reporter", "Size", "size of [a]"), auto_block("reporter", "Sqrt", "sqrt of [a]"), {
-        blockType: "command",
-        opcode: "RunJS",
-        text: "JS| Run JS code [code]",
-        arguments: {
-          code: {
-            type: "string",
-            defaultValue: "alert('Hello World!')"
+          {
+            blockType: "Boolean",
+            opcode: "booleanJS",
+            text: "js [code]",
+            arguments: {
+              code: {
+                type: "string",
+                defaultValue: "confirm('Are you sure?')"
+              },
+            },
           },
-        },
-      }, "---", {
-        blockType: "reporter",
-        opcode: "getReturnValOfJS",
-        text: "JS| Get return value of [code]",
-        arguments: {
-          code: {
-            type: "string",
-            defaultValue: "6473 / 84"
+          {
+            blockType: "command",
+            opcode: "functionCommand",
+            text: "run function [function] with args [ARGS]",
+            arguments: {
+              function: {
+                type: "string",
+                defaultValue: "function(a,b){return Number(a) + Number(b);}"
+              },
+              ARGS: {
+                type: "string",
+                defaultValue: "[2,5]"
+              }
+            }
           },
-        },
-      }, {
-        blockType: "command",
-        opcode: "OpenSite",
-        text: "JS| Open site [url]",
-        arguments: {
-          url: {
-            type: "string",
-            defaultValue: "https://example.com"
+          {
+            blockType: "reporter",
+            opcode: "functionReporter",
+            text: "run function [function] with args [ARGS]",
+            arguments: {
+              function: {
+                type: "string",
+                defaultValue: "function(a,b){return Number(a) + Number(b);}"
+              },
+              ARGS: {
+                type: "string",
+                defaultValue: "[2,5]"
+              }
+            }
           },
-        },
-      }, {
-        blockType: "command",
-        opcode: "SaveFile",
-        text: "JS| Save file [name] with contents [contents]",
-        arguments: {
-          name: {
-            type: "string",
-            defaultValue: "example.txt"
+          {
+            blockType: "Boolean",
+            opcode: "functionBoolean",
+            text: "run function [function] with args [ARGS]",
+            arguments: {
+              function: {
+                type: "string",
+                defaultValue: "function(a,b){return Number(a) + Number(b);}"
+              },
+              ARGS: {
+                type: "string",
+                defaultValue: "[2,5]"
+              }
+            }
           },
-          contents: {
-            type: "string",
-            defaultValue: "Hello World!"
+          {
+            blockType: "command",
+            opcode: "openInNewTab",
+            text: "open site [url] in new tab",
+            arguments: {
+              url: {
+                type: "string",
+                defaultValue: "https://example.com"
+              },
+            },
           },
-        },
-      }, {
-        blockType: "command",
-        opcode: "setVar",
-        text: "JS| Set variable [name] to [val]",
-        arguments: {
-          name: {
-            type: "string",
-            defaultValue: "window.example"
+          {
+            blockType: "command",
+            opcode: "redirect",
+            text: "redirect current tab to [url]",
+            arguments: {
+              url: {
+                type: "string",
+                defaultValue: "https://example.com"
+              },
+            },
           },
-          val: {
-            type: "string",
-            defaultValue: "Hello World!"
+          {
+            blockType: "command",
+            opcode: "openFile",
+            text: "open a [types] file",
+            arguments: {
+              types: {
+                type: "string",
+                defaultValue: "['.txt']"
+              }
+            }
           },
-        },
-      }, "---", "---", {
-        blockType: "hat",
-        opcode: "whenCondition",
-        text: "when [condit] is true",
-        isEdgeActivated: true,
-        arguments: {
-          condit: {
-            type: null,
+          {
+            blockType: "reporter",
+            opcode: "getFileData",
+            text: "last opened file [thing]",
+            arguments: {
+              thing: {
+                type: "string",
+                menu: "fileDataTypes"
+              }
+            },
+            disableMonitor: true,
           },
-        },
-      }, {
-        blockType: "reporter",
-        opcode: "stringReport",
-        text: "[arg1]",
-        arguments: {
-          arg1: {
-            type: "string",
-            defaultValue: "Hello"
+          {
+            blockType: "command",
+            opcode: "downloadFile",
+            text: "download file [contents] as [name]",
+            arguments: {
+              name: {
+                type: "string",
+                defaultValue: "example.txt"
+              },
+              contents: {
+                type: "string",
+                defaultValue: "Hello World!"
+              },
+            },
           },
-        },
-      }, {
-        blockType: "reporter",
-        opcode: "ifBoolStringElseString",
-        text: "if [arg1] then [arg2] else [arg3]",
-        arguments: {
-          arg1: {
-            type: "string",
-            defaultValue: "Boolean here"
+          {
+            blockType: "hat",
+            opcode: "whenCondition",
+            text: "when [bool] is true",
+            isEdgeActivated: true,
+            arguments: {
+              bool: {
+                type: "Boolean",
+              },
+            },
           },
-          arg2: {
-            type: "string",
-            defaultValue: "Hello"
+          {
+            blockType: "reporter",
+            opcode: "literal",
+            text: "[data]",
+            arguments: {
+              data: {
+                type: "string",
+                defaultValue: "Hello"
+              },
+            },
           },
-          arg3: {
-            type: "string",
-            defaultValue: "World"
+          {
+            blockType: "reporter",
+            opcode: "ifElseReturn",
+            text: "if [bool] then [val1] else [val2]",
+            arguments: {
+              bool: {
+                type: "Boolean"
+              },
+              val1: {
+                type: "string",
+                defaultValue: "Hello"
+              },
+              val2: {
+                type: "string",
+                defaultValue: "World"
+              },
+            },
           },
-        },
-      }, {
-        blockType: "reporter",
-        opcode: "outOfBoundsMouseX",
-        text: "Mouse X (works out of bounds)",
-        arguments: {},
-      }, {
-        blockType: "reporter",
-        opcode: "outOfBoundsMouseY",
-        text: "Mouse Y (works out of bounds)",
-        arguments: {},
-      }, ],
-      menus: {
-        varMenu: "getVarMenu"
-      },
-    };
-  }
-  getVarMenu(target_id) {
-    const vars = this.runtime.getTargetById(target_id).getAllVariableNamesInScopeByType("list");
-    return vars.length == 0 ? [" "] : vars;
-  }
-  Vec({
-    a,
-    b,
-    c
-  }) {
-    return to_s([
-      [a, b, c]
-    ]);
-  }
-  Out({
-    a,
-    b
-  }, util) {
-    let variable = util.target.lookupOrCreateList(undefined, b);
-    if (variable) variable.value = a.split(" ");
-  }
-  Get = mat_reporter_f(get);
-  Set = mat_reporter_f(set);
-  Arr = mat_reporter_f((a, b) => [...a, ...b]);
-  Rot = mat_reporter_f(rotate);
-  Add = mat_reporter_f(add2D);
-  Sub = mat_reporter_f(sub2D);
-  Mul = mat_reporter_f(mul2D);
-  Div = mat_reporter_f(div2D);
-  Dot = mat_reporter_f(dot);
-  Cross = mat_reporter_f(cross);
-  Len = mat_reporter_f(length);
-  Norm = mat_reporter_f(normalize);
-  Size = mat_reporter_f(m => [
-    [m.length]
-  ]);
-  Sqrt = mat_reporter_f(a => component_wise2D((a, b) => Math.sqrt(a))(a, [
-    [1]
-  ]));
-}
-
-function findReactComponent(element) {
-  let fiber = element[Object.keys(element).find(key => key.startsWith("__reactInternalInstance$"))];
-  if (fiber == null) return null;
-  const go = fiber => {
-    let parent = fiber.return;
-    while (typeof parent.type == "string") {
-      parent = parent.return;
+        ],
+        menus: {
+          fileDataTypes: {
+            acceptReporters: false,
+            items: ["content as text","content as dataURL","name","extension","size"]
+          }
+        }
+      };
     }
-    return parent;
-  };
-  fiber = go(fiber);
-  while (fiber.stateNode == null) {
-    fiber = go(fiber);
+    commandJS(args) {
+      eval(args.code);
+    }
+    reporterJS(args) {
+      return eval(args.code);
+    }
+    booleanJS(args) {
+      return eval(args.code)? true : false;
+    }
+    functionCommand(args) {
+      eval(`let func = ${args.function}; func(...JSON.parse(args.ARGS))`);
+    }
+    functionReporter(args) {
+      return eval(`let func = ${args.function}; func(...JSON.parse(args.ARGS))`);
+    }
+    functionBoolean(args) {
+      return eval(`let func = ${args.function}; func(...JSON.parse(args.ARGS))`)? true : false;
+    }
+    openInNewTab(args) {
+      window.open(args.url, '_blank');
+    }
+    redirect(args) {
+      window.location.href = args.url;
+    }
+    whenCondition(args) {
+      return args.bool? true : false;
+    }
+    literal(args) {
+      return args.data;
+    }
+    ifElseReturn(args) {
+      return args.bool? args.val1 : args.val2;
+    }
+    async openFile(args) {
+      promptForFile(JSON.parse(args.types))
+        .then(fileInfo => {
+          lastOpenedFileData = fileInfo;
+        })
+        .catch(e => {
+          console.error(error)
+        })
+    }
+    getFileData(args) {
+      switch (args.thing) {
+        case "content as text":
+          return lastOpenedFileData.contentAsText;
+        case "content as dataURL":
+          return lastOpenedFileData.contentAsDataURL;
+        case "name":
+          return lastOpenedFileData.name;
+        case "extension":
+          return lastOpenedFileData.extension;
+        case "size":
+          return lastOpenedFileData.size;
+        default:
+          return "";
+      }
+    }
+    downloadFile(args) {
+      _downloadFile(args.name, args.contents)
+    }
   }
-  return fiber.stateNode;
-}
-window.vm = (node => {
-  node = document.querySelector('div[class*=stage-header_stage-header-wrapper]');
-  node = node[Object.keys(node).find(key => (key = String(key), key.startsWith('__reactInternal') || key.startsWith('__reactFiber')))].return.return.return;
-  node = node.stateNode?.props?.vm || node.return?.updateQueue?.stores?.[0]?.value?.vm;
-  if (!node) throw new Error('Could not find VM :(');
-  return node;
-})();
-(function() {
-  var extensionInstance = new JavaScriptExt(window.vm.extensionManager.runtime);
-  var serviceName = window.vm.extensionManager._registerInternalExtension(extensionInstance);
-  window.vm.extensionManager._loadedExtensions.set(extensionInstance.getInfo().id, serviceName);
+  window.vm = (node => {
+    node = document.querySelector('div[class*=stage-header_stage-header-wrapper]');
+    node = node[Object.keys(node).find(key => (key = String(key), key.startsWith('__reactInternal') || key.startsWith('__reactFiber')))].return.return.return;
+    node = node.stateNode?.props?.vm || node.return?.updateQueue?.stores?.[0]?.value?.vm;
+    if (!node) throw new Error('Could not find VM :(');
+    return node;
+  })();
+  (function() {
+    var extensionInstance = new JSforScratch(window.vm.extensionManager.runtime);
+    var serviceName = window.vm.extensionManager._registerInternalExtension(extensionInstance);
+    window.vm.extensionManager._loadedExtensions.set(extensionInstance.getInfo().id, serviceName);
+  })();
 })();
