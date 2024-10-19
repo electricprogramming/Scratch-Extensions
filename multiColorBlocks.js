20c594b7d6901db33caca594f1fe37cb8cab5603
electricprogramming <yeungj316@gmail.com>
2024-10-19 18:30:13 -0400

    Update multiColorBlocks.js

--- multiColorBlocks.js
+++ multiColorBlocks.js
@@ -1,15 +1,15 @@
 (function (Scratch) {
   "use strict";
-  if (!Scratch.extensions.unsandboxed) throw new Error("MultiColor must run unsandboxed!");
+  if (!Scratch.extensions.unsandboxed) throw new Error("Custom Blocks Plus must run unsandboxed!");
   const vm = Scratch.vm;
   const runtime = vm.runtime;
-  /* adjusts converter to allow blocks to have their own color1, color2, and color3 properties. */
-  const ogConverter = runtime._convertBlockForScratchBlocks.bind(runtime);
+  /* Adjusts converter so blocks can have their own color1, color2, and color3 properties. */
+  const originalConverter = runtime._convertBlockForScratchBlocks.bind(runtime);
   runtime._convertBlockForScratchBlocks = function (blockInfo, categoryInfo) {
-    const res = ogConverter(blockInfo, categoryInfo);
+    const res = originalConverter(blockInfo, categoryInfo);
     if (blockInfo.color1) res.json.colour_ = blockInfo.color1;
     if (blockInfo.color2) res.json.colourSecondary_ = blockInfo.color2;
-    if (blockInfo.color3) res.json.colourTertiary = blockInfo.color3
+    if (blockInfo.color3) res.json.colourTertiary_ = blockInfo.color3;
     return res;
   }
   class epMultiColor {
@@ -17,8 +17,6 @@
       return {
         id: "epMultiColor",
         name: "MultiColor",
-        color1: "#7b9cbb",
-        color2: "#5a738a",
         blocks: [
           {
             opcode: 'red',
