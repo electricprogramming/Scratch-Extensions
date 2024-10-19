d00ed11cd4e46e43c330591500f0acd2bcafcf8c
electricprogramming <yeungj316@gmail.com>
2024-10-19 10:24:16 -0400

    Create vmGlobalizer.js

---
+++ nativeForScratch/vmGlobalizer.js
@@ -0,0 +1,14 @@
+(function(){
+  try {
+    window.vm = (node => {
+      node = document.querySelector('div[class*=stage-header_stage-header-wrapper]');
+      node = node[Object.keys(node).find(key => (key = String(key), key.startsWith('__reactInternal') || key.startsWith('__reactFiber')))].return.return.return;
+      node = node.stateNode?.props?.vm || node.return?.updateQueue?.stores?.[0]?.value?.vm;
+      if (!node) alert('Could not find VM.'); console.error('VM Globalizer could not find VM.')
+      return node;
+    })();
+  } catch(e) {
+    console.error('VM Globalizer could not find VM.',e)
+    alert('Could not find VM. See console for more details.')
+  }
+})();
